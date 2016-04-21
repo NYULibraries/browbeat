a3ecd5b30fde7662e2a7bd67402be3081cebbb4a
Eric Griffis <eric.griffis@nyu.edu>
2016-04-21 16:06:57 -0400

    URLs - extract URLs into yaml file loaded by separate helper

--- features/support/helpers/pds_helper.rb
+++ features/support/helpers/pds_helper.rb
@@ -1,31 +1,5 @@
 module Browbeat
   module PdsHelper
-    def current_host
-      URI::parse(current_url).host
-    end
-
-    def pds_url
-      "https://pds.library.nyu.edu/pds?func=load-login&institute=NYU"
-    end
-
-    def pds_lb_server_1_url
-      "https://primo1.bobst.nyu.edu/pds?func=load-login&institute=NYU"
-    end
-
-    def pds_lb_server_2_url
-      "https://primo2.bobst.nyu.edu/pds?func=load-login&institute=NYU"
-    end
-
-    def pds_staging_url
-      "https://pdsdev.library.nyu.edu/pds?func=load-login&institute=NYU"
-    end
-
-    def pds_staging_lb_server_1_url
-      "https://primodev1.bobst.nyu.edu/pds?func=load-login&institute=NYU"
-    end
-
-    def pds_staging_lb_server_2_url
-      "https://primodev2.bobst.nyu.edu/pds?func=load-login&institute=NYU"
-    end
+    
   end
 end
