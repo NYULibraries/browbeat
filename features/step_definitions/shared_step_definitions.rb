a03b72c09c461eff84443af791b784047976a9d0
Eric Griffis <eric.griffis@nyu.edu>
2016-04-28 15:44:33 -0400

    GetIt tests - add test for finding online access records; finishes #116795587

--- features/step_definitions/shared_step_definitions.rb
+++ features/step_definitions/shared_step_definitions.rb
@@ -2,6 +2,13 @@ Given(/^I visit (.+)$/) do |url_name|
   visit url_to url_name
 end
 
+When(/^I search for "(.*?)"$/) do |search_term|
+  within('form[name=searchForm]') do
+    fill_in("search_field", with: search_term)
+    click_on 'Search'
+  end
+end
+
 Then(/^my browser should respond with a success$/) do
   expect(page.find('body')).to have_content # expect(page.status_code).to eql 200
 end
