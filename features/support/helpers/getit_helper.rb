99cebd0e7c76e900ba6ae7dd5c7f4661299b60d5
Eric Griffis <eric.griffis@nyu.edu>
2016-05-02 15:26:27 -0400

    GetIt tests - change class var to instance var

--- features/support/helpers/getit_helper.rb
+++ features/support/helpers/getit_helper.rb
@@ -10,13 +10,13 @@ module Browbeat
 
     # captures a new window object opened by block for use with within_new_window
     def capture_new_window(&block)
-      @@new_window = window_opened_by &block
+      @new_window = window_opened_by &block
     end
 
     # executes given block within new window captured by capture_new_window
     def within_new_window(&block)
-      raise "Must capture_new_window before calling within_new_window" unless @@new_window
-      within_window @@new_window, &block
+      raise "Must capture_new_window before calling within_new_window" unless @new_window
+      within_window @new_window, &block
     end
   end
 end
