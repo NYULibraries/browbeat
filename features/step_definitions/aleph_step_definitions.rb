c868a15571251202951dc632b45cfa1bb8e2dee7
Eric Griffis <eric.griffis@nyu.edu>
2016-05-02 15:35:48 -0400

    Aleph tests - add ping and functionality tests

---
+++ features/step_definitions/aleph_step_definitions.rb
@@ -0,0 +1,22 @@
+When /^I search by keyword for "(.+)" in "(.+)"$/ do |keywords, field_type|
+  within "#keyword form" do
+    fill_in "request", with: keywords
+    select field_type, from: "find_code"
+    click_on "Search"
+  end
+end
+
+When /^I select the first result matching "(.+)"$/ do |result_text|
+  within first_aleph_result_matching(result_text) do
+    find('.holdinglink a').click
+  end
+end
+
+Then /^my browser should resolve to (Aleph.*)$/ do |aleph_url|
+  expect(page.current_url).to match url_to(aleph_url)
+  expect(page.current_path).to match /\/F\/[\d\w-]+/
+end
+
+Then /^I should see content matching "(.+)"$/ do |text|
+  expect(page).to have_content /#{text}/i
+end
