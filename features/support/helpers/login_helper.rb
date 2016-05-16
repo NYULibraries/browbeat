1b4a90569ffbbe7efeddbff8b4df7234f26a5bb4
Eric Griffis <eric.griffis@nyu.edu>
2016-05-16 14:25:02 -0400

    Login tests - add production credentials

--- features/support/helpers/login_helper.rb
+++ features/support/helpers/login_helper.rb
@@ -1,12 +1,12 @@
 module Browbeat
   module LoginHelper
     def shibboleth_username
-      Figs::ENV.nyu["staff"]["username"]
+      Figs::ENV.nyu["production_staff"]["username"]
     end
 
     def shibboleth_password
       raise "Cannot access production Shibboleth password while running tests in Sauce" if ENV['DRIVER'] == 'sauce'
-      Figs::ENV.nyu["staff"]["password"]
+      Figs::ENV.nyu["production_staff"]["password"]
     end
 
     def shibboleth_staging_username
