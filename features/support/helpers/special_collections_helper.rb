3c944d2ea0e37e2d31dd1c8be670f7f86918e204
Eric Griffis <eric.griffis@nyu.edu>
2016-06-09 14:46:38 -0400

    Fix bug in special collections

--- features/support/helpers/special_collections_helper.rb
+++ features/support/helpers/special_collections_helper.rb
@@ -13,7 +13,7 @@ module Browbeat
     end
 
     def web_solr_url
-      raise "Cannot access WebSolr URL while running tests in Sauce" #if ENV['DRIVER'] == 'sauce'
+      raise "Cannot access WebSolr URL while running tests in Sauce" if ENV['DRIVER'] == 'sauce'
       Figs::ENV.production['SOLR_URL']
     end
   end
