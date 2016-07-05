610578a0a8d470b3531700d5bdf27215051d7d24
Eric Griffis <eric.griffis@nyu.edu>
2016-07-05 15:00:05 -0400

    Replace urls yaml with environment variable

--- features/support/helpers/url_helper.rb
+++ features/support/helpers/url_helper.rb
@@ -1,11 +1,9 @@
 module Browbeat
   module UrlHelper
-    URL_CONFIG_FILEPATH = 'config/urls.yml' unless defined?(URL_CONFIG_FILEPATH)
-
-    # returns URL for given case-insensitive name as defined in YAML config file
+    # returns URL for given case-insensitive name as defined in URLS
     def url_to(url_name)
       url_name.downcase!
-      url_hash[url_name] || raise("URL \"#{url_name}\" is not defined. Define it in #{URL_CONFIG_FILEPATH}")
+      url_hash[url_name] || raise("URL \"#{url_name}\" is not defined. Define it in URLS")
     end
 
     def combine_url(*parts)
@@ -14,11 +12,7 @@ module Browbeat
 
     private
     def url_hash
-      @@url_hash ||= read_url_hash
-    end
-
-    def read_url_hash
-      YAML.load File.open(URL_CONFIG_FILEPATH){|f| f.read}
+      Figs::ENV["URLS"] || {}
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'coveralls'
 Coveralls.wear!
 require 'support/modify_env'
 require 'pry'
+require 'figs'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
