284627401f8b53fc8ccde1200cb72c4b0c651fed
Eric Griffis <eric.griffis@nyu.edu>
2016-09-21 11:29:59 -0400

    Loosen EZBorrow search test

--- features/step_definitions/ezborrow_step_definitions.rb
+++ features/step_definitions/ezborrow_step_definitions.rb
@@ -8,7 +8,7 @@ Then(/^my browser should resolve to EZBorrow$/) do
   expect(page.current_url).to eq "https://e-zborrow.relaisd2d.com/index.html"
 end
 
-Then(/^I should see EZBorrow results$/) do
-  expect(page).to have_content "Showing"
-  expect(ezborrow_results).to have_content
+Then(/^I should see EZBorrow results page$/) do
+  expect(page).to have_content "Sort by"
+  # expect(ezborrow_results).to have_content
 end
