a0617991f274479bd0981ca9d422b091865a554a
Eric Griffis <eric.griffis@nyu.edu>
2017-01-13 13:25:41 -0500

    Add tests

--- lib/browbeat/helpers/scrub_figs_helper.rb
+++ lib/browbeat/helpers/scrub_figs_helper.rb
@@ -2,15 +2,22 @@ module Browbeat
   module Helpers
     module ScrubFigsHelper
       def scrub_figs(text)
+        # binding.pry
         configula_data.inject(text) do |result, datum|
-          result.gsub(datum, 'X' * 10)
+          result.gsub(datum, 'X' * 5)
         end
       end
 
       private
 
       def configula_data
-        Figs::ENV["URLS"].values + [Figs::ENV.websolr['SOLR_URL'], Figs::ENV.production['SOLR_URL'], Figs::ENV.nyu["marli"]["password"], Figs::ENV.nyu["staff"]["password"], Figs::ENV.nyu["production_masters_student"]["password"]]
+        ((Figs::ENV["URLS"]&.values || []) + [
+          Figs::ENV.websolr&.fetch('SOLR_URL'),
+          Figs::ENV.production&.fetch('SOLR_URL'),
+          Figs::ENV.nyu&.fetch('marli')&.fetch('password'),
+          Figs::ENV.nyu&.fetch('staff')&.fetch('password'),
+          Figs::ENV.nyu&.fetch('production_masters_student')&.fetch('password')
+        ]).compact
       end
     end
   end
