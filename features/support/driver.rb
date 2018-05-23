cd00613e992e8e85c78db9ea36bb60c248932461
Eric Griffis <eric.griffis@nyu.edu>
2018-05-23 15:21:30 -0400

    Configure selenium to use chrome headless

--- features/support/driver.rb
+++ features/support/driver.rb
@@ -53,19 +53,65 @@ end
 SELENIUM_DOWNLOAD_FILETYPES = 'application/x-url' unless defined?(SELENIUM_DOWNLOAD_FILETYPES)
 
 # configure selenium to download to local directory
-def configure_selenium(browser)
+# def configure_selenium
+#   Capybara.register_driver :selenium do |app|
+#     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#       chromeOptions: {
+#         args: %w[ no-sandbox headless disable-gpu window-size=1280,1024]
+#       }
+#     )
+#
+#     Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+#   end
+# end
+
+def configure_selenium
   Capybara.register_driver :selenium do |app|
-    profile = Kernel.const_get("Selenium::WebDriver::#{browser.to_s.capitalize}::Profile").new
-    profile["browser.download.dir"] = File.join(FileUtils.pwd, ENV['SELENIUM_DOWNLOAD_DIRECTORY'])
-    profile["browser.download.folderList"] = 2
-    profile["browser.helperApps.neverAsk.saveToDisk"] = SELENIUM_DOWNLOAD_FILETYPES
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: browser.to_sym,
-      profile: profile,
-    )
+    options = Selenium::WebDriver::Chrome::Options.new
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-popup-blocking')
+    options.add_argument('--window-size=1280,1024')
+
+    options.add_preference(:download,
+      directory_upgrade: true,
+      prompt_for_download: false,
+      default_directory: ENV['SELENIUM_DOWNLOAD_DIRECTORY'])
+
+    options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
+
+    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+
+    bridge = driver.browser.send(:bridge)
+
+    path = '/session/:session_id/chromium/send_command'
+    path[':session_id'] = bridge.session_id
+
+    bridge.http.call(:post, path, cmd: 'Page.setDownloadBehavior',
+                                  params: {
+                                    behavior: 'allow',
+                                    downloadPath: ENV['SELENIUM_DOWNLOAD_DIRECTORY']
+                              })
+
+    driver
   end
 end
+
+# def configure_selenium(browser)
+#   Capybara.register_driver :selenium do |app|
+#     profile = Kernel.const_get("Selenium::WebDriver::#{browser.to_s.capitalize}::Profile").new
+#     profile["browser.download.dir"] = File.join(FileUtils.pwd, ENV['SELENIUM_DOWNLOAD_DIRECTORY'])
+#     profile["browser.download.folderList"] = 2
+#     profile["browser.helperApps.neverAsk.saveToDisk"] = SELENIUM_DOWNLOAD_FILETYPES
+#     Capybara::Selenium::Driver.new(
+#       app,
+#       browser: browser.to_sym,
+#       profile: profile,
+#     )
+#   end
+# end
 
 # if driver set to sauce, set and configure
 case ENV['DRIVER']
@@ -82,9 +128,11 @@ when nil
   Capybara.current_driver = :poltergeist
   Capybara.default_max_wait_time = (ENV['MAX_WAIT'] || 6).to_i
 # otherwise, run driver as a browser via selenium
-else
-  configure_selenium(ENV['DRIVER'])
+when 'chrome'
+  configure_selenium
   Capybara.default_driver = :selenium
   Capybara.javascript_driver = :selenium
   Capybara.default_max_wait_time = (ENV['MAX_WAIT'] || 15).to_i
+else
+  raise "Unrecognized driver '#{ENV['DRIVER']}'"
 end
