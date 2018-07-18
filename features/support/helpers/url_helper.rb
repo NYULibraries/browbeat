9de160df410e070811eeeb017ceb8a3445c95a03
Eric Griffis <eric.griffis@nyu.edu>
2018-07-18 12:36:57 -0400

    Correct urls error message

--- features/support/helpers/url_helper.rb
+++ features/support/helpers/url_helper.rb
@@ -3,7 +3,7 @@ module Browbeat
     # returns URL for given case-insensitive name as defined in URLS
     def url_to(url_name)
       url_name.downcase!
-      url_hash[url_name] || raise("URL \"#{url_name}\" is not defined. Define it in URLS")
+      url_hash[url_name] || raise("URL \"#{url_name}\" is not defined. Define it in #{url_path}")
     end
 
     def combine_url(*parts)
