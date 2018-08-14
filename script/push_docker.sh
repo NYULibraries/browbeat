b31e0bca355d90b1430f6753db25e147cc6fb71d
Eric Griffis <eric.griffis@nyu.edu>
2018-08-14 13:22:04 -0400

    Specify image name in docker compose

--- script/push_docker.sh
+++ script/push_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker tag browbeat_app nyulibraries/browbeat:latest
-docker tag browbeat_app nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}
-docker tag browbeat_app nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+docker tag browbeat nyulibraries/browbeat:latest
+docker tag browbeat nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}
+docker tag browbeat nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 
 docker push nyulibraries/browbeat:latest
 docker push nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}
