21365f4531c0f5ea209294389152ac8934068af2
Eric Griffis <eric.griffis@nyu.edu>
2018-08-14 13:03:49 -0400

    Use dockerized curl

--- script/pull_docker.sh
+++ script/pull_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function docker_tag_exists() {
-  curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
+  docker run --rm appropriate/curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
 }
 
 if docker_tag_exists nyulibraries/browbeat ${CIRCLE_BRANCH//\//_}; then
