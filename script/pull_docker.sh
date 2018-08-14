de556513ca64c37e915edf3e58d4cac8fafa294f
Eric Griffis <eric.griffis@nyu.edu>
2018-08-14 13:06:38 -0400

    Fix docker pull script

--- script/pull_docker.sh
+++ script/pull_docker.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-function docker_tag_exists() {
-  docker run --rm appropriate/curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-}
-
-if docker_tag_exists nyulibraries/browbeat ${CIRCLE_BRANCH//\//_}; then
-  docker pull nyulibraries/browbeat:${CIRCLE_BRANCH//\//_}
-else
-  docker pull nyulibraries/browbeat:latest
-fi
+docker pull nyulibraries/browbeat:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/browbeat:latest
