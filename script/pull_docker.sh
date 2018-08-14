7e61a2ca7d0161780dcd139ca295e987707ee06f
Eric Griffis <eric.griffis@nyu.edu>
2018-08-14 13:07:56 -0400

    Fix docker pull script temporarily

--- script/pull_docker.sh
+++ script/pull_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker pull nyulibraries/browbeat:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/browbeat:latest
+docker pull nyulibraries/browbeat:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/browbeat:master
