297cc1c4cc9ac967a1386f22809c709660b0bc22
Eric Griffis <eric.griffis@nyu.edu>
2019-01-23 15:59:08 -0500

    Circle - pull from quay

--- script/pull_docker.sh
+++ script/pull_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker pull nyulibraries/browbeat:${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/browbeat:latest
+docker pull quay.io/nyulibraries/browbeat:${CIRCLE_BRANCH//\//_} || docker pull quay.io/nyulibraries/browbeat:latest
